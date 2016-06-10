c20fa7ee2e884c3303a408c3436e7b24c63124c6
Samuel Navarrete <samueln.contact@gmail.com>
2016-06-10 14:07:07 -0700

    syntax changes

--- timeline/timeline.py
+++ timeline/timeline.py
@@ -10,18 +10,18 @@ class Timeline(object):
 
     def __init__(self, data=None, date_key='datetime'):
         self.date_key = date_key
-        self.data = data or []
+        self._data = data or []
 
-    def __filter_by(self, date=datetime.today(), compare_string='%Y%m%d'):
+    def _filter_by(self, date=datetime.today(), compare_string='%Y%m%d'):
         # Get date by datetime
         if isinstance(date, datetime):
-            return filter(lambda elem: elem.get(self.date_key) and elem.get(self.date_key).strftime(compare_string) == date.strftime(compare_string), self.data)
+            return filter(lambda elem: elem.get(self.date_key) and elem.get(self.date_key).strftime(compare_string) == date.strftime(compare_string), self._data)
 
         # Get date by string. ie %Y%m%d or %Y%m
         if isinstance(date, (basestring, str)) and len(date) == len(compare_string):
-            return filter(lambda elem: elem.get(self.date_key) and elem.get(self.date_key).strftime(compare_string) == date, self.data)
+            return filter(lambda elem: elem.get(self.date_key) and elem.get(self.date_key).strftime(compare_string) == date, self._data)
 
-    def __split_by(self, start_date=datetime.min, end_date=datetime.max, compare_string='%Y%m%d'):
+    def _split_by(self, start_date=datetime.min, end_date=datetime.max, compare_string='%Y%m%d'):
         statistics = {}
 
         for entry in self.filter_by_date(start_date, end_date):
@@ -41,21 +41,25 @@ class Timeline(object):
         # Return a dictionary of keys of the format compare_string. for example: statistics['%Y%m'] where compare_string is '%Y%m' of a specified date
         return statistics
 
+    @property
+    def data():
+        return self._data
+
     # Obtain a subset of the data representing a certain time interval
     def filter_by_date(self, start=datetime.min, end=datetime.max):
-        return filter(lambda elem: elem.get(self.date_key) and start <= elem.get(self.date_key) <= end, self.data)
+        return filter(lambda elem: elem.get(self.date_key) and start <= elem.get(self.date_key) <= end, self._data)
 
     def filter_by_day(self, date=datetime.today()):
-        return self.__filter_by(date, compare_string='%Y%m%d')
+        return self._filter_by(date, compare_string='%Y%m%d')
 
     def filter_by_month(self, date=datetime.today()):
-        return self.__filter_by(date, compare_string='%Y%m')
-
-    # Split data statistics by month. Search results by results['%Y%m']
-    def split_monthly(self, start_date=datetime.min, end_date=datetime.max):
-        return self.__split_by(start_date, end_date, '%Y%m')
+        return self._filter_by(date, compare_string='%Y%m')
 
     # Split data statistics by day. Search results by results['%Y%m%d']
     def split_daily(self, start_date=datetime.min, end_date=datetime.max):
-        return self.__split_by(start_date, end_date, '%Y%m%d')
+        return self._split_by(start_date, end_date, '%Y%m%d')
+
+    # Split data statistics by month. Search results by results['%Y%m']
+    def split_monthly(self, start_date=datetime.min, end_date=datetime.max):
+        return self._split_by(start_date, end_date, '%Y%m')
 
